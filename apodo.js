3cdd83b03766b68649c95763a639fc6bae206a12
fedelaurna <142011660+fedelaurna@users.noreply.github.com>
2025-09-17 08:17:40 -0300

    Add files via upload

---
+++ apodo.js
@@ -0,0 +1,85 @@
+let key="5hgcwemdorrZZQWCcgkqttIk7r8jH6KDy0qsTQes"
+
+let imagenDelDia = document.querySelector("#imagenDelDia")
+let boton = document.querySelector("#boton")
+let titulo = document.querySelector("h1")
+let info = document.querySelector("#infoImagenDelDia")
+let autor = document.querySelector("#autorImagenDelDia")
+
+ boton.onclick = function(){
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+.then(res => res.json())
+.then(fotoDelDia => {
+imagenDelDia.src = fotoDelDia.hdurl
+titulo.textContent = fotoDelDia.title
+ info.textContent = "Descripción: " + fotoDelDia.explanation
+  autor.textContent = "Autor: " + (fotoDelDia.copyright || "Desconocido")
+   })
+ }
+
+ let imagenFecha = document.querySelector("#imagenFecha")
+let botonFecha = document.querySelector("#botonFecha")
+let fechaUsuario = document.querySelector("#fecha")
+let infoFecha = document.querySelector("#infoImagenFecha")
+let autorFecha = document.querySelector("#autorImagenFecha")
+let tituloFecha = document.querySelector("#tituloImagenFecha")
+
+ botonFecha.onclick = function(){
+console.log(fechaUsuario)
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${fechaUsuario.value}`)
+.then(res => res.json())
+.then (fotoFecha => {
+imagenFecha.src = fotoFecha.hdurl
+   infoFecha.textContent = "Descripción: " + fotoFecha.explanation
+    autorFecha.textContent = "Autor: " + (fotoFecha.copyright || "Desconocido")
+      tituloFecha.textContent = fotoFecha.title
+  })
+ }
+
+ let contenedorImagen = document.querySelector(".contenedor-imagen")
+  let botonCantidad = document.querySelector("#botonCantidad")
+  let cantidadUsuario = document.querySelector("#cantidad")
+  
+
+  botonCantidad.onclick = function(){
+console.log(cantidadUsuario.value)
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&count=${cantidadUsuario.value}`)
+.then(res => res.json())
+.then(fotos => {
+contenedorImagen.innerHTML=""
+for (let i= 0; i < fotos.length; i++) {
+  contenedorImagen.innerHTML = `${contenedorImagen.innerHTML} <img src="${fotos[i].url}" alt="${fotos[i].title}">`
+   
+    }
+   })
+  }
+  let botonTresDias = document.querySelector("#botonTresDias")
+  let fechaInput = document.querySelector("#fechaTresDias")
+  let contenedor = document.querySelector("#contenedorTresDias")
+  
+  botonTresDias.onclick = () => {
+    contenedor.innerHTML = ""
+    if (!fechaInput.value) return alert("Elegí una fecha")
+  
+    let base = new Date(fechaInput.value)
+  
+    for (let i = 0; i <= 3; i++) {
+      let f = new Date(base)
+      f.setDate(f.getDate() - i)
+      let fecha = f.toISOString().split("T")[0]
+  
+      fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${fecha}`)
+        .then(res => res.json())
+        .then(data => {
+          contenedor.innerHTML += `
+            <div>
+              <h3>${data.title}</h3>
+              <img src="${data.url}" alt="" "><br>
+              <small>${data.explanation}</small>
+            </div>
+            <hr>
+          `
+        })
+    }
+  }
+  
